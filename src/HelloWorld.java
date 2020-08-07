792101060c1892030d3dce49efc401c788e4943b
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-07 17:00:11 -0500

    Java exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,41 +1,44 @@
 public class HelloWorld {
     public static void main(String[] args) {
-//        System.out.println("Hello, World!");
-//
-//        int myFavoriteNumber = 25;
-//        System.out.println(myFavoriteNumber);
-//
-//        String myString = "3.14159";
-//        System.out.println(myString);
-//
-//        double myNumber = 3.14;
-//        System.out.println(myNumber);
+        System.out.println("Hello, World!");
 
-//        int x = 5;
-//        System.out.println(x++);
-//        System.out.println(x);
+        int myFavoriteNumber = 25;
+        System.out.println(myFavoriteNumber);
 
-//        int x = 5;
-//        System.out.println(++x);
-//        System.out.println(x);
+        String myString = "3.14159";
+        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int y = 5;
+        System.out.println(++x);
+        System.out.println(x);
 
 //        String theNumberThree = "three";
 //        Object o = theNumberThree;
 //        int three = (int) o;
 //        int three = (int) "three";
 
-//        int x = 4;
-//        x += 5;
-//        System.out.println(x);
-//
-//        int x = 3;
-//        int y = 4;
-//        System.out.println(y *= x);
+        int z = 4;
+        z += 5;
+        System.out.println(z);
 
-//        int x = 10;
-//        int y = 2;
-//        System.out.println(x /= y);
-//        System.out.println(y -= x);
+        int a = 3;
+        int b = 4;
+        System.out.println(a *= b);
+
+        int c = 10;
+        int d = 2;
+        System.out.println(c /= d);
+        System.out.println(c -= d);
+
+        int coolExample = Integer.MAX_VALUE + 1;
+        System.out.println(coolExample);
 
     }
 }
