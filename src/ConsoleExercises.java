d52c86eae315ac16459404e9b721d4196e4bb29c
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-10 12:31:28 -0500

    exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -5,33 +5,31 @@ public class ConsoleExercises {
 
         //Printing the value of pie in 2 decimal place
 //        double pi = 3.14159;
-       // System.out.printf( "The value of pi is approximately " + "%.2f", pi);
+       // System.out.printf( "The value of pi is approximately " + "\n\n%.2f", pi);
 
         //Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
         Scanner scan = new Scanner(System.in);
-        scan.useDelimiter("\n");
+        //scan.useDelimiter("\n");
 //         int userInput = scan.nextInt();
 //        System.out.println(userInput + " is the number, that user typed");
 
         //Prompt a user to enter 3 words, and store each of them in a separate variable.
         // Then, display them back in the console, each on a newline.
 
-//        System.out.println("Please! enter a three words");
-//        String userInput1 = scan.nextLine();
-//        System.out.println(" You entered ---- " + userInput1);
-//        String userInput2 = scan.nextLine();
-//        System.out.println(" \n You entered ---- " + userInput2);
-//        String userInput3 = scan.nextLine();
-//        System.out.println(" \n You entered ---- " + userInput3);
+       System.out.println("Please! enter a three words");
+       String str1,str2,str3;
+        str1 = scan.nextLine();
+        str2 = scan.nextLine();
+        str3 = scan.nextLine();
+     System.out.println(" You entered ---- " + str1 +  str2 +  str3 );
 
         //Prompt a user to enter a sentence, then store that sentence in a String variable using the next method. Then, display that sentence back to the user.
-        //
         //do you capture all the words?
         //Rewrite the above example using the nextLine method.
 
 //        System.out.println("Please enter a sentence :");
 //        String printSentence = scan.nextLine();
-//        //if we use next() then it will not capture all the words because of whitespaces
+//        if we use next() then it will not capture all the words because of whitespaces
 //        System.out.println("you have entered  : " + printSentence);
 
 
@@ -43,10 +41,10 @@ public class ConsoleExercises {
         System.out.println("please enter the length of your classroom :");
         String userLength = scan.nextLine();
         double length = Double.parseDouble(userLength);
-        System.out.println("please enter the Width of your classroom" );
+        System.out.println("please enter the Width of your classroom :" );
         String userWidth = scan.nextLine();
         double width = Double.parseDouble(userWidth);
-        System.out.println("please enter the height of your classroom" );
+        System.out.println("please enter the height of your classroom :" );
         String userHeight = scan.nextLine();
         double height = Double.parseDouble(userHeight);
 
@@ -55,7 +53,7 @@ public class ConsoleExercises {
         // the perimeter of a rectangle is equal to 2 times the length plus 2 times the width.
 
         double areaOfClass = length * width ;
-        System.out.println("The area of classroom is : " + areaOfClass + " cm");
+        System.out.printf("The area of classroom is  :  %.2f" + areaOfClass + " cm");
 
         double perimeterOfClass = 2*(length + width);
         System.out.println("The perimeter of an classroom is : " + perimeterOfClass + " cm");
