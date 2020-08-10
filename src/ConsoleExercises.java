bba05ec2c201d588b8384ebd934a66ff6b751e6c
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-09 19:53:09 -0500

    exerciese

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double pi = 3.14159;
+        System.out.printf( "The value of pi is approximately " + "%.2f", pi);
+    }
+}
