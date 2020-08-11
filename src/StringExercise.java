5377096b2c29e444ca38e460b34c9a58d3dfb0c9
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-11 14:10:07 -0500

    exercises

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -7,17 +7,17 @@ public class StringExercise {
 //        it out to the console, you can do this with only one String variable and one print statement for each output
 //        example.
 
-        String output1 = "We don't need no education";
-        System.out.println(output1 + "\nWe don't need no thought control");
+        String output1 = "We don't need no education \n We don't need no thought control";
+        System.out.println(output1);
 
-        String output2 = "Check \"this\" out!,";
-        System.out.println(output2 + "\"s inside of \"s!");
+        String output2 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(output2);
 
-        String output3 = "In windows";
-        System.out.println(output3 + "the main drive is usually C:\\ " );
+        String output3 = "In windows the main drive is usually C:\\ ";
+        System.out.println(output3 );
 
-        String output4 = "I can do backlashes \\, double backlashes \\\\" ;
-        System.out.println(output4 + " and the amazing triple backlash \\\\\\\"");
+        String output4 = "I can do backlashes \\, double backlashes \\\\ and the amazing triple backlash \\\\\\\"";
+        System.out.println(output4 );
 
 
     }
