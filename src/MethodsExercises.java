34afb75a33d079ebbfba2a507ad31228325642da
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-12 16:52:17 -0500

    exercises

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,34 +1,62 @@
-
+import java.util.Scanner;
 public class MethodsExercises {
     public static void main(String[] args) {
-        System.out.println("The addition of 3 + 4 is : " + Addition(3, 4));
-        System.out.println("The subtraction of 5 - 2 is : " + Subtraction(5, 2));
-        System.out.println("The Multiplication of 5 *  7 is : " + Multiplication(5, 7));
-        System.out.println("The division of 50 by 2 is : " + Division(50, 2));
-        System.out.println("The modulus  of 11 by 4 is : " + Modulus(11, 2));
-
+        Scanner sc = new Scanner(System.in);
+//        System.out.println("The addition of 3 + 4 is : " + Addition(3, 4));
+//        System.out.println("The subtraction of 5 - 2 is : " + Subtraction(5, 2));
+//        System.out.println("The Multiplication is : " + Multiplication(2,5));
+//        System.out.println("The division of 50 by 2 is : " + Division(50, 2));
+//        System.out.println("The modulus  of 11 by 4 is : " + Modulus(11, 2));
+       // getInteger(2,8);
     }
 
     //Addition method
-    public static double Addition(double x, double y) {
-        return x + y;
-    }
+//    public static double Addition(double x, double y) {
+//        return x + y;
+//    }
+//    //Subtraction method
+//    public static double Subtraction(double x, double y) {
+//        return x - y;
+//    }
+//    // Multiplication method
+//    // Create your multiplication method without the * operator (Hint: a loop might be helpful)
+//    // Do the above with recursion.
+//    public static double Multiplication(double x, double y) {
+//        double result = 0;
+//        for (int i = 0; i < y; i++) {
+//            result += x;
+//        }
+//        return result;
+//    }
+//    //Division method
+//    public static double Division(double x, double y) {
+//        return x/ y;
+//    }
+//    //Modulus method
+//    public static double Modulus(double x, double y) {
+//        return x % y;
+//    }
 
-    //Subtraction method
-    public static double Subtraction(double x, double y) {
-        return x - y;
-    }
-    //Multiplication method
-    public static double Multiplication(double x, double y) {
-        return x * y;
-    }
-    //Division method
-    public static double Division(double x, double y) {
-        return x/ y;
-    }
-    //Modulus method
-    public static double Modulus(double x, double y) {
-        return x % y;
-    }
+    //Create a method that validates that user input is in a certain range
+//    public static int getInteger(int min, int max) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter a number between 1 and 10 to find maximum of two: ");
+//        int x = sc.nextInt();
+//        int y = sc.nextInt();
+//        if (x < y) { System.out.printf("Between %d and %d, Minimum Number is %d %n", x, y, x);
+//        } else { System.out.printf("Between %d and %d, Minimum is %d %n", x, y, y);
+//        }
+//
+//    }
+
+    //Calculate the factorial of a number
+//    Prompt the user to enter an integer from 1 to 10.
+//    Display the factorial of the number entered by the user.
+//    Ask if the user wants to continue.
+//    Use a for loop to calculate the factorial.
+//    Assume that the user will enter an integer, but verify it’s between 1 and 10.
+//    Use the long type to store the factorial.
+//    Continue only if the user agrees to.
+//    A factorial is a number multiplied by each of the numbers before it.
 
 }
