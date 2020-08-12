10d49e507814ae4e24ff05c7d8c0fd278c4b0bb9
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-12 11:04:57 -0500

    exercsies

---
+++ src/MethodsExercises.java
@@ -0,0 +1,29 @@
+
+public class MethodsExercises {
+    public static void main(String[] args) {
+        System.out.println(Addition(3, 4));
+        System.out.println(Subtraction(5, 2));
+        System.out.println(Multiplication(5, 7));
+        System.out.println(Division(50, 2));
+
+    }
+
+    //Addition method
+    public static double Addition(double x, double y) {
+        return x + y;
+    }
+
+    //Subtraction method
+    public static double Subtraction(double x, double y) {
+        return x - y;
+    }
+    //Multiplication method
+    public static double Multiplication(double x, double y) {
+        return x * y;
+    }
+    //Division method
+    public static double Division(double x, double y) {
+        return x/ y;
+    }
+
+}
