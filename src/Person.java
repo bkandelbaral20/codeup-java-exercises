e59b3dfa0a137c92b5c6ef738d193ea9a548dadc
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-13 16:46:07 -0500

    exerises

--- src/Person.java
+++ src/Person.java
@@ -2,11 +2,32 @@
 public class Person {
     private String name;
 
+    public Person(String name) {
+        this.name = name;
+    }
+
     public static void main(String[] args) {
         Person per = new Person();
         per.name = "Rama";
-
         System.out.println(per.name);
+
+        Person person1 = new Person("John");
+        Person person2 = new Person("John");
+        System.out.println(person1.getName().equals(person2.getName()));
+        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+
     }
     public String getName() {
 //TODO: return the person's name
