f97eabfd1a79aa01d2d9114332ce85871d4e5321
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-13 22:31:20 -0500

    exercises

---
+++ src/util/Input.java
@@ -0,0 +1,36 @@
+package util;
+import java.util.Scanner;
+
+public class Input {
+   private String scanner;
+
+      Scanner scan = new Scanner(System.in);
+
+//creating String get String() method
+   public String getString(){
+      return scanner;
+   }
+// boolean yesNo() method
+   public String yesNo(){
+      return scanner;
+   }
+
+   //int getInt(int min, int max)
+   public String getInt(int min, int max){
+      return scanner;
+   }
+
+   //int getInt()
+   public String getInt(){
+      return scanner;
+   }
+   //
+   public String getDouble(double min, double max){
+      return scanner;
+   }
+
+   //double getDouble()
+   public String getDouble(){
+      return scanner;
+   }
+}
