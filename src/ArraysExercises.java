f12be6c13cea687be1e6f7b6adac68395e4fa832
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-17 11:39:58 -0500

    exercises

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -5,16 +5,25 @@ public class ArraysExercises {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
 
-        //this print shows the location of an numbers array
+        //this print shows the location of an numbers array because it is an object
         System.out.println(numbers);
+        //this array to string will print all the array numbers
         System.out.println(Arrays.toString(numbers));
 
 
 //        Create an array that holds 3 Person objects. Assign a new instance of the Person class to each element.
 //        Iterate through the array and print out the name of each person in the array.
-        String[] person = {"Brian", "Austin", "Ryan"};
-        for (String n : person) {
-            System.out.println(n);
+        Person p1 =  new Person("Brian");
+        Person p2 =  new Person("Ryan");
+        Person p3 =  new Person("Austine");
+
+        Person [] people =  new Person[3];
+        people[0] = p1;
+        people[1] = p2;
+        people[2] = p3;
+
+        for (Person thisPerson : people) {
+            System.out.println(thisPerson.getName());
         }
     }
 
