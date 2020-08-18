1ebaeca16674990363bb7084fac11fedb8730b79
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-18 12:20:04 -0500

    exercises

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -2,24 +2,31 @@ package shapes;
 
 public class Rectangle {
 
-    //class variable
-    protected double length;
-    protected double width;
+//    1. previous exercises
+//    //class variable
+//    protected double length;
+//    protected double width;
+//
+//    //rectangle method for length and width
+//    public Rectangle(double length, double width) {
+//        this.length = length;
+//        this.width = width;
+//    }
+//
+//    //create getArea method
+//    public double getArea(double length, double width) {
+//        return length * width;
+//    }
+//
+//    //create getPerimeter method
+//    public double getPerimeter(double length, double width) {
+//        return 2 * (length + width);
+//    }
+    interface Measurable {
 
-    //rectangle method for length and width
-    public Rectangle(double length, double width) {
-        this.length = length;
-        this.width = width;
-    }
+        double getPerimeter();
 
-    //create getArea method
-    public double getArea(double length, double width) {
-        return length * width;
-    }
-
-    //create getPerimeter method
-    public double getPerimeter(double length, double width) {
-        return 2 * (length + width);
+        double getArea();
     }
 
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,21 +2,22 @@ package shapes;
 
 public class Square extends Rectangle {
 
-    public int sides;
-
-    //creating method for square
-    public Square(int sides){
-        super(sides, sides);
-        this.sides = sides;
-    }
-
-    //getArea and get method overriding
-    public double getArea(int sides){
-        return Math.pow(sides,2) ;
-    }
-    //create getPerimeter method
-    public double getPerimeter(int sides){
-        return  4 * sides;
-    }
+//    1.previous exercises
+//    public int sides;
+//
+//    //creating method for square
+//    public Square(int sides){
+//        super(sides, sides);
+//        this.sides = sides;
+//    }
+//
+//    //getArea and get method overriding
+//    public double getArea(int sides){
+//        return Math.pow(sides,2) ;
+//    }
+//    //create getPerimeter method
+//    public double getPerimeter(int sides){
+//        return  4 * sides;
+//    }
 
 }
