a127fffcb6ccd7e1590162cbb8abb22e8362f9ad
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-18 09:51:02 -0500

    exercises

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,17 +6,18 @@
 public class FizzBuzz {
     public static void fizzBuzz(int num) {
         for (int i = 1; i <= num; i++) {
-            if(num % 3 == 0) {
+            if (i % 3 == 0) {
                 System.out.println("Fizz");
-            } else if (num % 5 == 0) {
+            } else if (i % 5 == 0) {
                 System.out.println("Buzz");
-            } else if (num % 3 == 0 && num % 5 == 0) {
+            } else if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
             } else {
-                System.out.println(num);
+                System.out.println(i);
             }
         }
     }
+
     public static void main(String[] args) {
         fizzBuzz(50);
 
