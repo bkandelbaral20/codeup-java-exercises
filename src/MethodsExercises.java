b536fd977b6501a88d2ef1f201ff6a4cfa77fa31
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-26 12:51:56 -0500

    exercises

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class MethodsExercises {
@@ -6,23 +8,25 @@ public class MethodsExercises {
 
 //        System.out.println("The addition of 3 + 4 is : " + Addition(3, 4));
 //        System.out.println("The subtraction of 5 - 2 is : " + Subtraction(5, 2));
-//        System.out.println("The Multiplication is : " + Multiplication(2,5));
+//        System.out.println("The Multiplication is : " + Multiplication(2, 5));
 //        System.out.println("The division of 50 by 2 is : " + Division(50, 2));
 //        System.out.println("The modulus  of 11 by 4 is : " + Modulus(11, 2));
-//        System.out.println("The integer is  : " + getInteger(1,10));
-//        System.out.println(factorialNumber(1,10));
-//        // System.out.println(MultiplicationRecursively(5,3));
-//    }
+//        System.out.println("The integer is  : " + getInteger(1, 10));
+//        System.out.println("The factorial of 9 is " + factorialNumber(9));
+//        System.out.println(MultiplicationRecursively(5, 3));
+    }
 
-        //Addition method
+    //Addition method
 //    public static double Addition(double x, double y) {
 //        return x + y;
 //    }
-//    //Subtraction method
+
+    //Subtraction method
 //    public static double Subtraction(double x, double y) {
 //        return x - y;
 //    }
-//    // Multiplication method
+
+    //    // Multiplication method
 //    // Create your multiplication method without the * operator (Hint: a loop might be helpful)
 //    // Do the above with recursion.
 //    public static double Multiplication(double x, double y) {
@@ -33,26 +37,28 @@ public class MethodsExercises {
 //        return result;
 //    }
 
-//    public static int  MultiplicationRecursively (int a, int b){
+//    public static int MultiplicationRecursively(int a, int b) {
 //        int product = 0;
-//        if(a>0){
-//            product =  (b + MultiplicationRecursively(a-1,b));
-//        }else if ( a<0){
-//            product = (0-b) + MultiplicationRecursively(a+1,b);
+//        if (a > 0) {
+//            product = (b + MultiplicationRecursively(a - 1, b));
+//        } else if (a < 0) {
+//            product = (0 - b) + MultiplicationRecursively(a + 1, b);
 //        }
 //        return product;
 //
 //    }
-//    //Division method
+
+    //    //Division method
 //    public static double Division(double x, double y) {
-//        return x/ y;
+//        return x / y;
 //    }
-//    //Modulus method
+
+    //    //Modulus method
 //    public static double Modulus(double x, double y) {
 //        return x % y;
 //    }
 
-        //Create a method that validates that user input is in a certain range
+    //Create a method that validates that user input is in a certain range
 //    public static int getInteger(int min, int max) {
 //        // i.e. see if number is between 1 and 10
 //        Scanner scanner = new Scanner(System.in);
@@ -62,17 +68,17 @@ public class MethodsExercises {
 //        do {
 //            num = scanner.nextInt();
 //            if (num < min || num > max) {
-//                System.out.printf("\nThe integer %d is out of range\n", num);
+//                System.out.printf("The integer %d is out of range\n", num);
 //                System.out.printf("Enter an integer between %d and %d: ", min, max);
 //            } else {
-//                doAgain = true; // they did enter a number within range, so break while
+//                doAgain = true;// they did enter a number within range, so break while
 //            }
 //        } while (doAgain); // if we get a correct number (1 < x < 10), break out of while
 //        return num;
 //    }
 
 
-        //Calculate the factorial of a number
+    //Calculate the factorial of a number
 //    Prompt the user to enter an integer from 1 to 10.
 //    Display the factorial of the number entered by the user.
 //    Ask if the user wants to continue.
@@ -82,17 +88,15 @@ public class MethodsExercises {
 //    Continue only if the user agrees to.
 //    A factorial is a number multiplied by each of the numbers before it.
 
-//    public static long factorialNumber(int i, int num) {
-//        System.out.print("Enter a number between 1 and 10 : ");
-//        int factorial = 1;
-//        int previousFactorial = 1;
-//        Scanner scan = new Scanner(System.in);
-//        int input = scan.nextInt();
-//        for (int i = 1; i <= input; ++i) {
-//            input = input * i;
-//        }
-//        return factorial;
-//    }
+    public static long factorialNumber(int num)  {
+        // factorial(5) = 5 * 4 * 3 * 2 * 1
+        int factorial = 1;
+        int previousFactorial = 1;
+        for (int i = 1; i <= num; ++i) {
+            factorial = factorial * i;
+        }
+        return  factorial;
+    }
 
 //    Create an application that simulates dice rolling.
 //    Ask the user to enter the number of sides for a pair of dice.
@@ -103,5 +107,4 @@ public class MethodsExercises {
 //    Use the .random method of the java.lang.Math class to generate random numbers.
 
 
-    }
 }
