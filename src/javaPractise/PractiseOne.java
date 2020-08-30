b31939de9e03174e62261bd5945ac1a4fd57e5f5
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-29 22:47:34 -0500

    completed exercises

--- src/javaPractise/PractiseOne.java
+++ src/javaPractise/PractiseOne.java
@@ -2,32 +2,29 @@ package javaPractise;
 
 //Take three numbers from the user and print the greatest number.
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
+import java.lang.Math;
 
 public class PractiseOne {
-    public static void  main(String[] args) {
-        System.out.println("Please enter 3 numbers");
+    public static void main(String[] args) {
+        System.out.println("Please enter 3 numbers on each lines");
         Scanner scan = new Scanner(System.in);
-        int firstNumber = scan.nextInt();
-        System.out.println("The first number is " + firstNumber);
-        int secondNumber = scan.nextInt();
-        System.out.println("The second number is " + secondNumber);
-        int thirdNumber = scan.nextInt();
-        System.out.println("The third number is " + thirdNumber);
 
-        // calculate which one is highest
-        int highest;
-        if (firstNumber > secondNumber) {
-            highest = firstNumber;
-        } else {
-            highest = secondNumber;
+    // calculate which one is highest using Math.max
+        int x = Integer.parseInt(scan.nextLine());
+        int y = Integer.parseInt(scan.nextLine());
+        int z = Integer.parseInt(scan.nextLine());
+
+        //Math.max only compares 2 values
+        int max = Math.max(x,y);
+        if(max>y){ //suppose x is max then compare x with z to find max number
+            max = Math.max(x,z);
         }
-
-        // Now check between max and c
-        if (thirdNumber > highest) {
-            highest = thirdNumber;
+        else{ //if y is max then compare y with z to find max number
+            max = Math.max(y,z);
         }
-
-        return highest;
+        System.out.println("The max of three numbers is: " + max);
     }
 }
